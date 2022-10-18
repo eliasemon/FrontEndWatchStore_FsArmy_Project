90613795f70fcaf6ab0cf33b0b27dbc4dbe00a59
MH Murshed <arifsdarif@gmail.com>
2022-10-18 21:08:02 +0600

    font family property added in css

--- components/globalStyles/index.jsx
+++ components/globalStyles/index.jsx
@@ -8,9 +8,14 @@ const GlobalStyle = createGlobalStyle`
   border: none;
   font-family: "Montserrat", sans-serif;
 }
+
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
   -webkit-appearance: none;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
 }
 `;
 export default GlobalStyle;
