be7ee6a496e812958331a7e478f8a916f24c1216
MH Murshed <arifsdarif@gmail.com>
2022-10-22 18:25:41 +0600

    object moved and tried to re-use same component

--- components/UI/texts/index.jsx
+++ components/UI/texts/index.jsx
@@ -1,28 +1,18 @@
-import styled, {css} from "styled-components";
-
-const fontSizes = {
-  sm: "0.8rem",
-  md: "1.2rem",
-  lg: "1.65rem",
-  xl: "3.75rem",
-};
-
-const lineHeights = {
-  sm: "1.2",
-  md: "1.4",
-  lg: "1.6",
-};
+import styled, { css } from "styled-components";
 
 const Text = css`
-  font-size: ${(props) => fontSizes[props.size] ?? "1rem"};
-  color: ${(props) => props.color ?? "black"};
-  line-height: ${(props) => lineHeights[props.line] ?? 1};
+  font-size: ${({ size, theme }) => theme.fontSizes[size] ?? "1rem"};
+  color: ${({ color, theme }) =>
+    color === "primary" ? theme?.color?.primaryColor : theme?.color?.textColor};
+  line-height: ${({ line, theme }) => theme.lineHeights[line] ?? 1};
+  font-weight: ${({weight, theme}) => theme?.fontWeight[weight] ?? theme?.fontWeight['regular']};
+  text-decoration: ${(props) => props.cross ? 'line-through' : 'none'};
 `;
 
 export const BlockText = styled.p`
   ${Text}
-`
+`;
 
 export const InlineText = styled.span`
   ${Text}
-`
+`;
