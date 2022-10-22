a2aace6795c31153e4d91bccb15ab88054971aa1
MH Murshed <arifsdarif@gmail.com>
2022-10-22 18:42:23 +0600

    rating component added

---
+++ components/UI/cards/rating.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { FaStar } from "react-icons/fa";
+
+const Star = styled.span`
+  color: #ffb61f;
+  font-size: 1.5rem;
+  margin-right: .5rem;
+`;
+
+export const SingleStar = () => {
+  return (
+    <Star>
+      <FaStar />
+    </Star>
+  );
+};
+
+export const Multiple = styled.div`
+  margin: .5rem 0;
+  letter-spacing: 1rem;
+`
+
+export const Rating = ({ rates }) => {
+  const stars = [];
+  let i = 0;
+  while (i < rates) {
+    stars.push(<SingleStar />);
+    i++;
+  }
+  console.log(stars);
+  return <Multiple>{stars}</Multiple>;
+};
