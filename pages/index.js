c32a576754c3ffb286ac17f2bb8215583bdce977
MH Murshed <arifsdarif@gmail.com>
2022-10-22 23:45:19 +0600

    theme added in index page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@
 // import styles from '../styles/Home.module.css'
 
 import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import theme from './theme';
 
 export default function Home() {
   return (
@@ -11,8 +13,10 @@ export default function Home() {
         <title>Watch Store #Team-F</title>
         <link rel="icon" href="/watch-favicon.ico" />
       </Head> */}
-
+      <ThemeProvider theme={theme}>
       <h1> Hello Watch Store </h1>
+      </ThemeProvider>
+
     </>
   );
 }
